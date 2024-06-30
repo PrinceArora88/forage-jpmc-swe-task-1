505f7e678e4603322dbc89131e07b0fb9f80f116
Roya <royaunlock@gmail.com>
2024-06-30 16:31:10 +0430

    getRatio

--- client3.py
+++ client3.py
@@ -40,9 +40,12 @@ def getDataPoint(quote):
 
 
 def getRatio(price_a, price_b):
+    if (price_b == 0):
+        #when price_b is 0 avoid throwing ZeroDivisionError
+        return
     """ Get ratio of price_a and price_b """
     """ ------------- Update this function ------------- """
-    return 1
+    return price_a/price_b
 
 
 # Main
